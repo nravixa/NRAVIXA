62f8bb1526acf6b1ccd1e6ee63de7c0e08264587
Neha Raut <your-github-email@example.com>
2026-07-03 20:08:00 +0530

    Small Change

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from '@prisma/config'
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  earlyAccess: true,
-  schema: {
-    path: './prisma/schema.prisma',
+  schema: "./prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL || "postgresql://placeholder:5432/db",
   },
-  migrate: {
-    databaseUrl: process.env.DATABASE_URL || 'postgresql://placeholder:5432/db',
-  },
-})
+});
